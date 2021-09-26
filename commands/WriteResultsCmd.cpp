fbcd3145d6a2afef75a39ecb09b4e1f6063a46ef
ItamarGefen <88192967+ItamarGefen@users.noreply.github.com>
2021-09-26 12:30:07 +0300

    -changed PrintResultsCmd to WriteResultsCmd, and modified it to use DefaultIO

--- commands/WriteResultsCmd.cpp
+++ commands/WriteResultsCmd.cpp
@@ -2,16 +2,16 @@
 // Created by user on 9/25/2021.
 //
 
-#include "PrintResultsCmd.h"
+#include "WriteResultsCmd.h"
 
-void PrintResultsCmd::execute() {
+void WriteResultsCmd::execute() {
     vector<string>* results = this->classifier->getResults();
 
     int i = 1;
     for (const string& s : *results) {
         string toPrint = std::to_string(i) + "\t" + s;
-        std::cout << toPrint << std::endl;
+        this->dio->write(toPrint);
         i++;
     }
-    std::cout << "Done." << std::endl;
+    this->dio->write("Done.");
 }
--- commands/WriteResultsCmd.h
+++ commands/WriteResultsCmd.h
@@ -2,21 +2,22 @@
 // Created by user on 9/25/2021.
 //
 
-#ifndef HW3_PRINTRESULTSCMD_H
-#define HW3_PRINTRESULTSCMD_H
+#ifndef HW3_WRITERESULTSCMD_H
+#define HW3_WRITERESULTSCMD_H
 
 #include "ICommand.h"
 
 using std::vector;
 
-class PrintResultsCmd : public ICommand {
+class WriteResultsCmd : public ICommand {
 private:
     string cmdDescription = "display results";
     IClassifier* classifier;
+    DefaultIO* dio;
 
 public:
     void execute() override;
 };
 
 
-#endif //HW3_PRINTRESULTSCMD_H
+#endif //HW3_WRITERESULTSCMD_H
