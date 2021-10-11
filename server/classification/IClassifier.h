c633886a1cbde90bd7ebe75c1ab072cc18a786fa
ItamarGefen <88192967+ItamarGefen@users.noreply.github.com>
2021-10-11 14:55:54 +0300

    - removed unused methods getTraningdata getTestingdata

--- server/classification/IClassifier.h
+++ server/classification/IClassifier.h
@@ -18,15 +18,15 @@ public:
     virtual void classifyAllTestingData() = 0;
     virtual vector<string>* getResults() const = 0;
     virtual vector<std::pair<string, vector<double>>> calculateConfusionMatrix() const = 0;
+
     /*************GETTERS AND SETTERS***************/
     virtual int getK() const = 0;
     virtual void setK(int k) = 0;
-
     virtual void setTrainingData(vector<Classifiable*>* data) = 0;
-
     virtual void setTestingData(vector<Classifiable*>* data) = 0;
     virtual string getMetricName() const = 0;
     virtual void setDistanceCalculatingMethod(const string& type) = 0;
+
     /** virtual destructor **/
     virtual ~IClassifier() = default;
 };
